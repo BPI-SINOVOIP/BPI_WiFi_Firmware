6dee7aabad92112e548b551c5acb9611d15e5b33
Justin <Justin>
2017-07-21 08:08:15 +0800

    UpLoad New AP6212 FW A1_Version

--- ap6212/device-bcm.mk
+++ ap6212/device-bcm.mk
@@ -18,12 +18,16 @@
 -include hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk
 
 WIFI_DRIVER_FW_PATH_STA    := "/system/vendor/modules/fw_bcm43438a0.bin"
-WIFI_DRIVER_FW_PATH_P2P    := "/system/vendor/modules/fw_bcm43438a0_apsta.bin"
-WIFI_DRIVER_FW_PATH_AP     := "/system/vendor/modules/fw_bcm43438a0_p2p.bin"
+WIFI_DRIVER_FW_PATH_AP     := "/system/vendor/modules/fw_bcm43438a0_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P    := "/system/vendor/modules/fw_bcm43438a0_p2p.bin"
 
 PRODUCT_COPY_FILES += \
     hardware/broadcom/wlan/bcmdhd/firmware/ap6212/fw_bcm43438a0.bin:system/vendor/modules/fw_bcm43438a0.bin \
     hardware/broadcom/wlan/bcmdhd/firmware/ap6212/fw_bcm43438a0_apsta.bin:system/vendor/modules/fw_bcm43438a0_apsta.bin \
     hardware/broadcom/wlan/bcmdhd/firmware/ap6212/fw_bcm43438a0_p2p.bin:system/vendor/modules/fw_bcm43438a0_p2p.bin \
     hardware/broadcom/wlan/bcmdhd/firmware/ap6212/nvram_ap6212.txt:system/vendor/modules/nvram.txt \
-    hardware/broadcom/wlan/bcmdhd/firmware/ap6212/bcm43438a0.hcd:system/vendor/modules/bcm43438a0.hcd
+    hardware/broadcom/wlan/bcmdhd/firmware/ap6212/bcm43438a0.hcd:system/vendor/modules/bcm43438a0.hcd \
+    hardware/broadcom/wlan/bcmdhd/firmware/ap6212/fw_bcm43438a1.bin:system/vendor/modules/fw_bcm43438a1.bin \
+    hardware/broadcom/wlan/bcmdhd/firmware/ap6212/fw_bcm43438a1_apsta.bin:system/vendor/modules/fw_bcm43438a1_apsta.bin \
+    hardware/broadcom/wlan/bcmdhd/firmware/ap6212/fw_bcm43438a1_p2p.bin:system/vendor/modules/fw_bcm43438a1_p2p.bin \
+    hardware/broadcom/wlan/bcmdhd/firmware/ap6212/bcm43438a1.hcd:system/vendor/modules/bcm43438a1.hcd 
